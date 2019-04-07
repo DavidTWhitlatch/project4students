b874d6dbb28f8c9cf606a655e5121cc4f0146926
David Whitlatch <davidtwhitlatch@gmail.com>
2019-04-07 15:57:00 -0400

    removed express

--- express4testing/routes/homeRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const homeRouter = express.Router();
-
-homeRouter.get('/', (req, res) => res.send('You guys are amazing!'))
-
-
-module.exports = homeRouter;
--- express4testing/scratch.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-
-(async () => {
-  const resp = await axios.put('http://localhost:3001/food/4/flavors/3');
-  console.log(resp.data)
-  process.exit();
-})();
