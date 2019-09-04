1e04e0a85797a741f7b16dbdbaed727db7b3385a
David Whitlatch <davidtwhitlatch@gmail.com>
2019-09-04 10:28:23 -0400

    updated Models file

--- express4testing/models.js
+++ express4testing/models.js
@@ -1,21 +1,25 @@
 const Sequelize = require('sequelize');
+const { Model } = require('sequelize');
+
 const sequelize = new Sequelize({
   database: 'testing4testing',
   dialect: 'postgres',
   operatorsAliases: false,
   define: {
     underscored: true,
-    returning: true
-  }
+    returning: true,
+  },
 });
 
-const Food = sequelize.define('food', {
+class Food extends Model {}
+Food.init({
   name: Sequelize.STRING,
-})
+}, { sequelize, modelName: 'food' });
 
-const Flavor = sequelize.define('flavor', {
+class Flavor extends Model {}
+Flavor.init({
   name: Sequelize.STRING,
-})
+}, { sequelize, modelName: 'flavor' });
 
 // Here we define our many to many associations
 // This creates a new table called 'food_flavors' as a join table
@@ -24,14 +28,16 @@ Food.belongsToMany(Flavor, { through: 'food_flavors' });
 Flavor.belongsToMany(Food, { through: 'food_flavors' });
 
 // Also setting up a 'user' table for auth with 'pasword_digest'. NOT 'password'!
-const User = sequelize.define('user', {
+
+class User extends Model {}
+User.init({
   username: Sequelize.STRING,
-  password_digest: Sequelize.STRING
-})
+  password_digest: Sequelize.STRING,
+}, { sequelize, modelName: 'user' });
 
 module.exports = {
   Food,
   Flavor,
   sequelize,
-  User
+  User,
 };
